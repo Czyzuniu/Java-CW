dd760b45c4719d62fce327705ead068312135e09
Greg <Greg@10.128.4.170>
2016-11-04 13:24:39 +0000

    change

--- Java Coursework/src/InputReader.java
+++ Java Coursework/src/InputReader.java
@@ -25,7 +25,7 @@ public InputReader()
 			
 			while(!orderCompleted)
 			{
-				System.out.println("hello how are you ?? : ");
+				System.out.println("hiii yoo : ");
 				width = reader.nextInt();
 				System.out.println("Please enter a height : ");
 				height = reader.nextInt();
